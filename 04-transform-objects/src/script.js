dd052788edb765babf5a9a08e7ff16f5eb557ba6
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-12-05 21:56:20 +0100

    0.4 (group)

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -10,31 +10,33 @@ const scene = new THREE.Scene()
  * Objects
  */
 
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+scene.add(group)
 
-// Position
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
-mesh.position.set(0.7, -0.6, 1) // kan ook zo
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
 
-// mesh.position.normalize() // makes lenght vector 1
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = -2
+group.add(cube2)
 
-// Scale
-// mesh.scale.x = 2
-// mesh.scale.y = 0.5
-// mesh.scale.z = 0.5
-mesh.scale.set(2, 0.5, 0.5)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 2
+group.add(cube3)
 
-
-// Rotation
-mesh.rotation.reorder('YXZ') // moet eerst y en dan x, anders krijg je een gimbel lock
-mesh.rotation.x = Math.PI * .25
-mesh.rotation.y = Math.PI * .25
 
 
 // Axis helper
