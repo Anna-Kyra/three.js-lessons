c63040710742503f86f51b633e2613533fc39d8f
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-12-05 21:49:46 +0100

    0.4 (begin uitleg)
    - mesh.position
    - mesh.rotation
    -

---
+++ 04-transform-objects/src/script.js
@@ -0,0 +1,69 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Position
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+mesh.position.set(0.7, -0.6, 1) // kan ook zo
+
+// mesh.position.normalize() // makes lenght vector 1
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2, 0.5, 0.5)
+
+
+// Rotation
+mesh.rotation.reorder('YXZ') // moet eerst y en dan x, anders krijg je een gimbel lock
+mesh.rotation.x = Math.PI * .25
+mesh.rotation.y = Math.PI * .25
+
+
+// Axis helper
+
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+
+// console.log(mesh.position.distanceTo(camera.position)) //distance between camera and cube
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera) // it takes a picture, everything after this point doesn't work
