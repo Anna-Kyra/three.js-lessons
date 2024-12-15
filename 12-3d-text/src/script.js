2bbbaa22e9c2f6529cdf192901cb7e1eb84378bf
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2024-12-15 15:40:17 +0100

    12. (Optimize)
    console.time('donuts')
    
       /* Insert object */
    
    console.timeEnd('donuts')
    
    Je kan zien in de console hoeveel tijd het in beslag neemt om het object wat hier binnen staat te renderen
    
    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
            const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
    
    > zet deze uit de for function, want je hoeft dit niet vaker dan 1x te maken, scheelt performance

--- 12-3d-text/src/script.js
+++ 12-3d-text/src/script.js
@@ -77,9 +77,13 @@ fontLoader.load(
         const text = new THREE.Mesh(textGeometry, textMaterial)
         scene.add(text)
 
+        console.time('donuts')
+        
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+        const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
         for(let i = 0; i < 100; i++){
-            const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
-            const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+            
             const donut = new THREE.Mesh(donutGeometry, donutMaterial)
 
             donut.position.x = (Math.random() - 0.5) * 10
@@ -98,6 +102,7 @@ fontLoader.load(
 
             scene.add(donut)
         }
+        console.timeEnd('donuts')
     }
 )
 
