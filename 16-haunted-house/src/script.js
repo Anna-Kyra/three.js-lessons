5c8e04dfe3089aebc339c387ac861e2584ed34f1
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-01-24 22:30:41 +0100

    16. (Fog)
    Three.js supports 2 fog features: Fog en FogExp2
    
    scene.fog = new THREE.Fog('#04343f', 10, 13)
    > Fog heeft twee perentisies nodig en de fog begint pas bij de 10 far.
    
    scene.fog = new THREE.FogExp2('#04343f', 0.1)
    > FogExp2 is meer realistisch, de fog heeft geen harde lijn, heeft maar 1 prentisy nodig en de fog heeft naar mate het op de voorgrond zit steeds meer transparantie

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -295,6 +295,11 @@ sky.material.uniforms['mieCoefficient'].value = 0.1
 sky.material.uniforms['mieDirectionalG'].value = 0.95
 sky.material.uniforms['sunPosition'].value.set(0.3, -0.038, -0.95)
 
+/**
+ * Fog
+ */
+// scene.fog = new THREE.Fog('#04343f', 10, 13)
+scene.fog = new THREE.FogExp2('#04343f', 0.1)
 
 
 /**
