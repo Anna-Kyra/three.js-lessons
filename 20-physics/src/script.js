35cb895347f70311912f766c0e24ada3820811ae
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-02-24 22:30:34 +0100

    20. (Remove things)
    Reset button om de geometries te verwijderen
    
    debugObject.reset = () => {
        for(const object of objectsToUpdate){
            // Remove
            object.body.removeEventListener('collide', playHitSound)
            world.removeBody(object.body)
    
            // Remove mesh
            scene.remove(object.mesh)
        }
        objectsToUpdate.splice(0, objectsToUpdate.length) // empty de array
    }
    
    gui.add(debugObject, 'reset')

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -35,8 +35,21 @@ debugObject.createBox = () => {
     )
 }
 
+debugObject.reset = () => {
+    for(const object of objectsToUpdate){
+        // Remove
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+
+        // Remove mesh
+        scene.remove(object.mesh)
+    }
+    objectsToUpdate.splice(0, objectsToUpdate.length) // empty de array
+}
+
 gui.add(debugObject, 'createSphere')
 gui.add(debugObject, 'createBox')
+gui.add(debugObject, 'reset')
 
 /**
  * Base
