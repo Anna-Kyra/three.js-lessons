5e94fbddc5e781a9acae11c634e8df070087422e
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-03-01 12:14:32 +0100

    22. (Test on each frame)
    Let op! testen op elk frame is slecht voor performance!!
    Maar als je wilt testen wanneer objecten bewegen moet je op elk frame testen.
    
    // Animate objects
        object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
        object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
        object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
    > De ballen laten bewegen in de tick functie
    
        // Cast a ray
        const rayOrigin = new THREE.Vector3(-3, 0, 0)
        const rayDirection = new THREE.Vector3(1, 0, 0)
        rayDirection.normalize() // Beter om altijd de direction te normaliseren voor als iemand de direction veranderd
    
        raycaster.set(rayOrigin, rayDirection)
    
        const objectsToTest = [object1, object2, object3]
        const intersects = raycaster.intersectObjects(objectsToTest)
        console.log(intersects.length)
    > Voor moving objects is de raycaster intersect hetzelfde, maar dan in de tick function
    
        for(const object of objectsToTest){
            object.material.color.set('red') // wanneer de raycaster niet intersect zijn de ballen rood
        }
    
        for(const intersect of intersects){
            intersect.object.material.color.set('blue') // met alleen deze loop blijft de ballen blauw omdat ze beginnen in het midden intersect bij de raycaster
        }
    > Kleur van object veranderen wanneer de object de raycaster intersect

--- 22-raycaster-and-mouse-events/src/script.js
+++ 22-raycaster-and-mouse-events/src/script.js
@@ -45,17 +45,17 @@ object3.updateMatrixWorld()
 
 const raycaster = new THREE.Raycaster()
 
-const rayOrigin = new THREE.Vector3(-3, 0, 0)
-const rayDirection = new THREE.Vector3(10, 0, 0) 
-rayDirection.normalize()// je wilt dat de raydirection gelijk is aan 1
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0) 
+// rayDirection.normalize()// je wilt dat de raydirection gelijk is aan 1
 
-raycaster.set(rayOrigin, rayDirection)
+// raycaster.set(rayOrigin, rayDirection)
 
-const intersect = raycaster.intersectObject(object2) // 1 object
-console.log(intersect)
+// const intersect = raycaster.intersectObject(object2) // 1 object
+// console.log(intersect)
 
-const intersects = raycaster.intersectObjects([object1, object2, object3]) // array objects
-console.log(intersects)
+// const intersects = raycaster.intersectObjects([object1, object2, object3]) // array objects
+// console.log(intersects)
 
 /**
  * Sizes
@@ -109,6 +109,34 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+
+
+    // Cast a ray
+    const rayOrigin = new THREE.Vector3(-3, 0, 0)
+    const rayDirection = new THREE.Vector3(1, 0, 0)
+    rayDirection.normalize() // Beter om altijd de direction te normaliseren voor als iemand de direction veranderd
+
+    raycaster.set(rayOrigin, rayDirection)
+
+    const objectsToTest = [object1, object2, object3]
+    const intersects = raycaster.intersectObjects(objectsToTest)
+
+    for(const object of objectsToTest){
+        object.material.color.set('red') // wanneer de raycaster niet intersect zijn de ballen rood
+    }
+
+    for(const intersect of intersects){
+        intersect.object.material.color.set('blue') // met alleen deze loop blijft de ballen blauw omdat ze beginnen in het midden intersect bij de raycaster
+    }
+
+    
+
+    
 
     // Update controls
     controls.update()
