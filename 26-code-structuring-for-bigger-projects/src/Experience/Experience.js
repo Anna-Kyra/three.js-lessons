a869224f17c02c81bfacd2df0bcb64f2d47e9cfe
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-04-18 21:10:44 +0200

    26. (Useful classes)
    Sizes:
    Creer een 'Utils'map in de 'Exerience'map.
    Hier kunnen useful classes zitten bv sizes
    
    import Sizes from './Utils/Sizes.js'
    export default class Experience {
        constructor(canvas) {
            // Global access
            window.experience = this
    
            // Option
            this.canvas = canvas
            console.log(this.canvas)
    
            // Sizes
            this.sizes = new Sizes()
        }
    }
    > In Experience.js
    
    export default class Sizes {
        constructor() {
            console.log('jup')
        }
    }
    > in Utlis > Sizes.js
    
    export default class Sizes {
        constructor() {
            // Setup
            this.width = window.innerWidth
            this.height = window.innerHeight
            this.pixelRatio = Math.min(window.devicePixelRatio, 2)
    
            // Resize event
            window.addEventListener('resize', () => {
                this.width = window.innerWidth
                this.height = window.innerHeight
                this.pixelRatio = Math.min(window.devicePixelRatio, 2)
            })
        }
    }
    > Wat erin de sizes moet
    
    Event emitter:
    We willen de Sizes class to trigger events en later lateren we het luisteren naar andere events.
    Dit doe je met een EventEmitter.
    Deze Eventemitter heeft Bruno zelf gemaaakt en gebruikt hij in al zijn werk zowat.
    
    https://gist.github.com/brunosimon/120acda915e6629e3a4d497935b16bdf
    > Deze code uit de RAW file copieren en toevoegen in Utils > EventEmitter.js
    
    import EventEmitter from "./EventEmitter.js"
    
    export default class Sizes extends EventEmitter {
        constructor() {
            super() // omdat het een inheritence class is
    
    > moet de EventEmitter importeren en de Sizes class een inheritance class maken.
    Omdat het een inheritence class is moet je de `super()` function gebruiken
    
    // Resize event
            window.addEventListener('resize', () => {
                this.width = window.innerWidth
                this.height = window.innerHeight
                this.pixelRatio = Math.min(window.devicePixelRatio, 2)
    
                this.trigger('resize')
            })
    >  in de Sizes.js kan je de reisze triggeren
    
            // Sizes
            this.sizes = new Sizes()
    
            this.sizes.on('resize', () => {
                console.log('I heard a resize')
            })
    > Zo roep je hem aan in de Experience.js. Je kan de resize alles noemen, moet wel in de sizes.js en experience.js hetzelfde genoemd worden
    
    Time:
    import EventEmitter from "./EventEmitter.js"
    
    export default class Time extends EventEmitter {
        constructor() {
            super()
    
            // Setup
            this.start = Date.now()
            this.current = this.start
            this.elapsed = 0
            this.delta = 16 // want default screen run 16fps
    
            window.requestAnimationFrame(() => {
                this.tick()
            })
        }
    
        tick() {
            const currentTime = Date.now()
            this.delta = currentTime - this.current
            this.current = currentTime
            this.elapsed = this.current - this.start
    
            this.trigger('tick')
    
            window.requestAnimationFrame(() => {
                this.tick()
            })
        }
    }
    > in de Utils > Time.js file
    
    import Time from './Utils/Time.js'
    
    export default class Experience {
        constructor(canvas) {
            // Global access
            window.experience = this
    
            // Option
            this.canvas = canvas
            console.log(this.canvas)
    
            // Setup
            this.sizes = new Sizes()
            this.time = new Time()
    
            // Sizes resize event
            this.sizes.on('resize', () => { // moet fat arrow gebruiken om de context niet te verliezen
                this.resize()
            })
    
            // Time tick event
            this.time.on('tick', () => {
                this.update()
            })
        }
    
        resize() {
    
        }
    
        update() {
        }
    }
    > In de Experience.js file
    
    De EventEmitter kan gebruikt worden voor elke classe dat grotere events nodig heeft zoals:
    - een animation waneer een model finished
    - Een object is op geklikt
    - De player gaat naar een nieuw level
    - Een enemy died

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,3 +1,6 @@
+import Sizes from './Utils/Sizes.js'
+import Time from './Utils/Time.js'
+
 export default class Experience {
     constructor(canvas) {
         // Global access
@@ -6,5 +9,26 @@ export default class Experience {
         // Option
         this.canvas = canvas
         console.log(this.canvas)
+
+        // Setup
+        this.sizes = new Sizes()
+        this.time = new Time()
+
+        // Sizes resize event
+        this.sizes.on('resize', () => { // moet fat arrow gebruiken om de context niet te verliezen
+            this.resize()
+        })
+
+        // Time tick event
+        this.time.on('tick', () => {
+            this.update()
+        })
+    }
+
+    resize() {
+        
+    }
+
+    update() {
     }
 }
