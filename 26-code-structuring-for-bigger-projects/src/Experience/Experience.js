bed38b582e5765bd8ace5f29f641660447fdbc4e
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-04-18 21:54:49 +0200

    26. (Camera (and Singleton))
    Voor de camera heb je acces nodig bij het canvas in de experience.js.
    
    Je kan dit op 3 manieren doen.
    - Van global variable
    - Bij sending een parameter
    - Bij gebruiken van singleton
    
    Global variable:
    export default class camera {
        constructor() {
            this.experience = window.experience
            console.log(window.experience.sizes.width)
        }
    }
    > Kan met de window, maar meestal niet fijn want je kan external code up fucken. Beetje messy
    
    Parameters:
    this.camera = new Camera(this)
    > this event toevoegen in Experience.js
    
    constructor(experience) {
            this.experience = experience
            console.log(experience.sizes.width)
        }
    > experience event toevoegen in Camera.js
    
    Singleton:
    Een class dat instantiate gewoon, gewoon als eerste keer, maar voor de volgende keer gaat het de first instance returnen
    
    let instance = null
    > in Experience.js boven de export
    
        constructor(canvas) {
            // Singleton
            if(instance){ // is de instance er al
                return instance
            }
            instance = this
    > in de construtor als de instance aangeroepen wordt returnen
    
    export default class camera {
        constructor(experience) {
            this.experience = new Experience()
            console.log(this.experience)
        }
    }
    > zo roep je de singleton aan in de Camera.js
    
    export default class Camera {
        constructor(experience) {
            this.experience = new Experience()
            this.sizes = this.experience.sizes
            this.scene = this.experience.scene
            this.canvas = this.experience.canvas
        }
    }
    > Alles wat je nodig hebt erin zetten van de experience
    
    setInstance:
    import * as THREE from 'three'
    > Hebt three.js nodig, dus three importeren
    
        setInstance() {
            this.instance = new THREE.PerspectiveCamera(
                35,
                this.sizes.width / this.sizes.height,
                0.1,
                100
            )
            this.instance.position.set(6, 4, 8)
            this.scene.add(this.instance)
        }
    > in de export van de camera class de setInstance tovoegen
    
        constructor(experience) {
            this.experience = new Experience()
            this.sizes = this.experience.sizes
            this.scene = this.experience.scene
            this.canvas = this.experience.canvas
    
            this.setInstance()
        }
    > IN de constructor de `setInstance()` function aanroepen
    
    Orbitcontrols:
    import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
    
    > import OrbitControls in Camera.js
    
        setOrbitControls() {
            this.controls = new OrbitControls(this.instance, this.canvas) // camera, canvas
            this.controls.enableDamping = true
        }
    > Voeg OrbitControls + damping toe in de export van de Camera class
    
    Resize:
        resize() {
            this.camera.resize()
        }
    > Roep resize aan in de Experience.js
    
        resize() {
            this.instance.aspect = this.sizes.width / this.sizes.height
            this.instance.updateProjectionMatrix()
        }
    > doe resize in de Camera.js en update de matrix
    
    Update:
        update() {
            this.camera.update()
        }
    > update de camera in de experience.js
    
        update() {
            this.controls.update()
        }
    > update de controls in de camera.js

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,9 +1,18 @@
 import * as THREE from 'three'
 import Sizes from './Utils/Sizes.js'
 import Time from './Utils/Time.js'
+import Camera from './Camera.js'
+
+let instance = null
 
 export default class Experience {
     constructor(canvas) {
+        // Singleton
+        if(instance){ // is de instance er al
+            return instance
+        }
+        instance = this
+
         // Global access
         window.experience = this
 
@@ -15,6 +24,7 @@ export default class Experience {
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
+        this.camera = new Camera()
 
         // Sizes resize event
         this.sizes.on('resize', () => { // moet fat arrow gebruiken om de context niet te verliezen
@@ -28,9 +38,10 @@ export default class Experience {
     }
 
     resize() {
-        
+        this.camera.resize()
     }
 
     update() {
+        this.camera.update()
     }
 }
