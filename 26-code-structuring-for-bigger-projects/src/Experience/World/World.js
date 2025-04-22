dc6aba10ef795ac7cea105db458ddacdb7086cfe
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-04-22 09:36:17 +0200

    26. (Fox)
    import * as THREE from 'three'
    import Experience from '../Experience.js'
    
    export default class Fox {
        constructor() {
            this.experience = new Experience()
            this.scene = this.experience.scene
            this.resources = this.experience.resources
            this.time = this.experience.time
    
            // Setup
            this.resource = this.resources.items.foxModel
    
            this.setModel()
            this.setAnimation()
        }
    
        setModel() {
            this.model = this.resource.scene
            this.model.scale.set(0.02, 0.02, 0.02)
            this.scene.add(this.model)
    
            this.model.traverse((child) => {
                if(child instanceof THREE.Mesh) {
                    child.castShadow = true
                }
            })
        }
    
        setAnimation() {
            this.animation = {}
            this.animation.mixer = new THREE.AnimationMixer(this.model)
            this.animation.action = this.animation.mixer.clipAction(this.resource.animations[0])
            this.animation.action.play()
        }
    
        update() {
            this.animation.mixer.update(this.time.delta * 0.001) // want delta is in milliseconds en de animiotion is in seconds
        }
    }
    > dit in de Fox.js in de World map
    
        {
            name: 'foxModel',
            type: 'gltfModel',
            path: 'models/Fox/glTF/Fox.gltf'
        }
    > De gltf model moet is een bestand en moet in de resources worden gezet
    
        update() {
            this.camera.update()
            this.world.update()
            this.renderer.update()
        }
    > In Experience moet de world geupdate worden
    
    import Fox from './Fox.js'
    
        update() {
            if(this.fox) {
                this.fox.update()
            }
        }
    > Bij de World.js moet de fox geupdate worden en de fox moet geimporteerd worden

--- 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import Experience from "../Experience.js"
 import Environment from './Environment.js'
 import Floor from './Floor.js'
+import Fox from './Fox.js'
 
 export default class World {
     constructor() {
@@ -10,20 +11,25 @@ export default class World {
         this.resources = this.experience.resources 
 
         // Test mesh
-        const testMesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshStandardMaterial()
-        )
-        this.scene.add(testMesh)
+        // const testMesh = new THREE.Mesh(
+        //     new THREE.BoxGeometry(1, 1, 1),
+        //     new THREE.MeshStandardMaterial()
+        // )
+        // this.scene.add(testMesh)
 
         // Wait for resources
         this.resources.on('ready', () => {
             // Setup
             this.floor = new Floor() // floor moet eerst om part of the scene zijn. nu werkt de intensity enzo wel
+            this.fox = new Fox()
             this.environment = new Environment() // inside want als je de environment map gebruikt gebruik je de resources
             
-        })
+        }) 
+    }
 
-        
+    update() {
+        if(this.fox) {
+            this.fox.update()
+        }   
     }
 }
