a34a0dbf1afa81ea3d4977534a8db17788e64770
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-06-12 11:04:11 +0200

    27. (GLSL) part 1
    De shaders worden in GLSL (OpenGL Shading Language) geschreven, dit is dicht bij de C language.
    
    Je kan geen console.log of console.print gebruiken hierin.
    Er is geen console.
    
    De indentation is niet essentieel
    
    Semicolons ';' zijn important!!
    Je moet het gebruiken
    
    Float:
        float a = 1.0;
        float b = 2.0;
        float c = a + b;
    
    > Bij GLSL kan je geen fouten schrijven. Een float (decimaal is daar werkelijk een float.
    Je kan ook calculate gebruiken zoals hierboven in GLSL.
    
    Inter:
        int a = 1;
        int b = 2;
        int c = a * b;
    
    > Je kan ook int gebruiken (geen decimalen, maar niet is niet handig om te gebruiken. Als je gaat delen dan kan je misschien een float krijgen en dat is dan niet handig.
    
    !! Je kan float en int niet mixen met elkaar
    
        float a = 1.0;
        int b = 2;
        float c = a * float(b);
    > Je kan een int wel vormen naar een float met float(...)
    
    Boolean:
        bool foo = true;
        bool bar = false;
    > zo schrijf je een boolean, wordt niet echt vaak gebruikt
    
    Vector2:
        vec2 foo = vec2(1.0 , 2.0);
    
    > Je moet hiervoor 2 coordinaten hebben, want x en y
    
    vec2 foo = vec2(0.0);
    > Je kan niet niks of null in je vec2 hebben, er moet echt een cijfer in.
    
        vec2 foo = vec2(0.0);
        foo.x = 1.0;
        foo.y = 2.0;
    > Zo kan je de x en y apart benoemen
    
        vec2 foo = vec2(1.0, 2.0);
        foo *= 2.0;
    > Zo kan je de vec2 coordinaten multiplyen
    
    Vector3:
        vec3 foo = vec3(0.0);
        vec3 bar = vec3(1.0, 2.0, 3.0);
        bar.z = 4.0;
    > vec3 heeft dus 3 coordinaten x, y en z. Je kan ze dus ook veranderen later.
    
        vec3 purpleColor = vec3(0.0);
        purpleColor.r = 0.5;
        purpleColor.g = 1.0;
    > Je kan ook r, g, b als alliase gebruiken
    
        vec2 foo = vec2(1.0, 2.0);
        vec3 bar = vec3(foo, 3.0);
    > Je kan Vector 2 en 3 ook combineren met elkaar
    
        vec3 foo = vec3(1.0, 2.0, 3.0)
        vec2 bar = foo.xy
    > Je kan op deze manieer ook een vec2 van een vec3 maken. Dit noem je 'swizzle'

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -5,5 +5,9 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 
 void main() {
+    vec3 foo = vec3(1.0, 2.0, 3.0)
+    vec2 bar = foo.xy
+
+
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
