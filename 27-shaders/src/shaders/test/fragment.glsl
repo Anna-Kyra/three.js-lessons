bbc025cbc767f8a01264f6df3e852b5d9842d3dd
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-06-19 17:23:52 +0200

    27.(Color variations)
    Met color variations kan de shadow gemimiked worden.
    
        float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
        elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
    
        modelPosition.z += elevation;
    > De wind animatie  wordt als een variabelen gemaakt in de vertex shader.
    
    varying float vElevation;
    
    void main() {
        vElevation = elevation;
    }
    > Dit moet een shadow maken, dus het moet naar de fragment shader gestuurd worden doormiddel van een varying.
    
    varying float vElevation;
    void main() {
        vec4 textureColor = texture2D(uTexture, vUv); //vec4 omdat je ook transparante afbeeldingen kan gebruiken
        textureColor.rgb *= vElevation * 2.0 + 0.6;
        gl_FragColor = textureColor;
    }
    > Je gebruikt alleen de rgb en niet de alpha.

--- 27-shaders/src/shaders/test/fragment.glsl
+++ 27-shaders/src/shaders/test/fragment.glsl
@@ -4,8 +4,10 @@ uniform vec3 uColor;
 uniform sampler2D uTexture;
 
 varying vec2 vUv;
+varying float vElevation;
 
 void main() {
     vec4 textureColor = texture2D(uTexture, vUv); //vec4 omdat je ook transparante afbeeldingen kan gebruiken
+    textureColor.rgb *= vElevation * 2.0 + 0.6;
     gl_FragColor = textureColor;
 }
